cf2d3aa1f63d64edaec9f0407c6e71fbc4b9fad9
bahigabu <sbz1212lucky@gmail.com>
2026-05-12 18:30:22 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>我的 Todo List</h1>
+
+    <input v-model="newTodo" placeholder="輸入代辦事項" />
+    <button @click="addTodo">新增</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+
+function addTodo() {
+  console.log(newTodo.value)
+}
+</script>

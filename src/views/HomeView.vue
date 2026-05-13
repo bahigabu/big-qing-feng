91dbd69e62ad28be4ecc7acd73b80b750a025da2
bahigabu <sbz1212lucky@gmail.com>
2026-05-13 20:17:12 +0800

    router fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>首頁</h1>
-  <a href="http://localhost:5173/about">關於頁</a>
+  <a href="https://big-qing-feng.vercel.app/about">關於頁</a>
 </template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,5 @@
 <template>
   <h2>個人資料頁</h2>
-  <a href="http://localhost:5173/about/profile/settings">設定頁</a>
+  <a href="https://big-qing-feng.vercel.app/about/profile/settings">設定頁</a>
   <router-view />
 </template>
